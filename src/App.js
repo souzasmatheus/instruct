7630c5da885461b56b3364aab44cfb491d2ecf25
Matheus Souza <matheu.sk.9@hotmail.com>
2020-01-31 21:44:08 -0300

    Style App component and implement logic to fetch data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import './App.scss';
 import Card from './components/Card';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await Axios.get(
+          'http://jsonplaceholder.typicode.com/users'
+        );
+
+        console.log(result);
+
+        setUsers(result.data);
+      } catch (e) {
+        // setError('An error occurred. Please, reload application.');
+      }
+    })();
+  }, []);
+
+  const cards = users.map(
+    ({ name, username, email, address, phone, website }, index) => (
+      <Card
+        key={`card-${index + 1}`}
+        name={name}
+        username={username}
+        email={email}
+        address={address}
+        phone={phone}
+        website={website}
+      />
+    )
+  );
+
   return (
     <div className="App">
-      <Card
-        name="Matheus"
-        username="souzasmatheus"
-        email="matheu.sk.9@hotmail.com"
-        telephone="22988144983"
-        website="www.souzasmatheus.com.br"
-        address="Rua Prudente de Moraes, 299"
-      />
+      <header className="input-label-container">
+        <label htmlFor="query">Insira um sufixo de e-mail:</label>
+        <div className="input-btn-container">
+          <p>.</p>
+          <input type="text" placeholder="" />
+        </div>
+      </header>
+
+      <section className="cards-container">{cards}</section>
     </div>
   );
 }
